bf43bd9f166df2f686f2bf14c039f8414ecd43e7
yoricktf <yorick.tenfeld@gmail.com>
2022-03-14 21:48:58 +0100

    working on authentication problems

---
+++ client/src/pages/SignupPage.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+export default function Signup() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('+++++++++++++++++++++++++++++++++++++++');
+    const requestBody = { email, password, name }
+    axios.post('/auth/signup', requestBody)
+      .then(response => {
+        // redirect to login
+        navigate('/login')
+      })
+      .catch(err => {
+        const errorDescription = err.response.data.message
+        setErrorMessage(errorDescription)
+      })
+  }
+
+  const test = () => {
+    console.log('test');
+    axios.get('/auth/test')
+  }
+
+  const handleEmail = e => setEmail(e.target.value)
+  const handleName = e => setName(e.target.value)
+  const handlePassword = e => setPassword(e.target.value)
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  return (
+    <>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email: </label>
+        <input type="text" value={email} onChange={handleEmail} />
+        <label htmlFor="password">Password: </label>
+        <input type="password" value={password} onChange={handlePassword} />
+        <label htmlFor="name">Name: </label>
+        <input type="text" value={name} onChange={handleName} />
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {errorMessage && <h5>{errorMessage}</h5>}
+
+      <h1 onClick={test}>test</h1>
+
+      <h3>Already have an account?</h3>
+      <Link to='/login'>Login</Link>
+    </>
+  )
+}
--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,10 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    name: { type: String, unique: true, required: true },
+    profilePicture: String,
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
