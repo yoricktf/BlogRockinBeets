b7a032e8a453935f79fb7d91411b63fa25f0b4fb
yoricktf <yorick.tenfeld@gmail.com>
2022-03-14 10:09:04 +0100

    added router and initial pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import RecipePage from './pages/RecipePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React duh
-        </a>
-      </header>
+      <h1>Main page</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes/:id" element={<RecipePage />} />
+
+      </Routes>
+
+
+
     </div>
   );
 }
