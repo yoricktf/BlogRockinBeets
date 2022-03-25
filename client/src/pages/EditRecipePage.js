cffbee9e8419a2b88c9ac2f085afb42f8d2dfcdc
yoricktf <yorick.tenfeld@gmail.com>
2022-03-25 21:34:30 +0100

    set the response to the state and then set the value, problem with getting the arrays

--- client/src/pages/EditRecipePage.js
+++ client/src/pages/EditRecipePage.js
@@ -27,16 +27,30 @@ const EditRecipePage = () => {
   const [tags, setTags] = useState([])
   const [author, setAuthor] = useState(user)
   const [published, setPublished] = useState(false)
-  const [recipe, setRecipe] = useState({})
+  // const [recipe, setRecipe] = useState({})
   // -----------------------------------------------
   const { id } = useParams()
 
-  console.log(recipe);
+  // console.log(recipe);
 
 
   const getSpecificRecipe = async () => {
     const response = await axios.post('/recipe/specificRecipe', { id })
-    setRecipe(response.data)
+    const { recipeName, ingredients, recipePicture, description, method, prepTime, cookTime, servingSize, difficulty, author, tags, published } = response.data
+    // setRecipe(response.data)
+    // setRecipeName(response.data.recipeName)
+    setRecipeName(recipeName)
+    setIngredients(ingredients)
+    setDescription(description)
+    setRecipePicture(recipePicture)
+    setMethod(method)
+    setPrepTime(prepTime)
+    setCookTime(cookTime)
+    setServingSize(servingSize)
+    setDifficulty(difficulty)
+    setTags(tags)
+    setAuthor(author)
+    setPublished(published)
   }
 
   const editRecipe = (e) => {
@@ -57,8 +71,6 @@ const EditRecipePage = () => {
 
 
 
-
-
   const handleFileUpload = e => {
     const uploadData = new FormData();
     uploadData.append("recipePicture", e.target.files[0]);
@@ -75,10 +87,13 @@ const EditRecipePage = () => {
   }, [])
 
 
+  const handleRecipeNameChange = event => {
+    setRecipeName(event.target.value)
+  }
 
 
-
-
+  console.log('----------', ingredients);
+  console.log('++++++++++++++', method);
 
   return (
     <>
@@ -87,12 +102,12 @@ const EditRecipePage = () => {
         <Form onSubmit={editRecipe}>
           <Form.Group className="mb-3" controlId="recipeName">
             <Form.Label>Recipe Name</Form.Label>
-            <Form.Control value={recipe.recipeName} onChange={e => setRecipeName(e.target.value)} type="text" placeholder="enter ingredient and add here" />
+            <Form.Control value={recipeName} onChange={handleRecipeNameChange} type="text" placeholder="enter ingredient and add here" />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="description">
             <Form.Label>Description</Form.Label>
-            <Form.Control value={recipe.description} onChange={e => setDescription(e.target.value)} type="text" placeholder="enter method step by step" />
+            <Form.Control value={description} onChange={e => setDescription(e.target.value)} type="text" placeholder="enter method step by step" />
           </Form.Group>
 
           <input type="file" onChange={(e) => handleFileUpload(e)} />
@@ -103,7 +118,8 @@ const EditRecipePage = () => {
               enter the ingredients seperated by 2 percentge signs ( %% )
             </Form.Text>
             <Form.Control
-              value={recipe.ingredients?.join('%%')}
+              value={ingredients?.join('%%')}
+              // value={ingredients}
               onChange={e => setIngredients(e.target.value.split('%%'))}
               type="text"
             />
@@ -115,7 +131,8 @@ const EditRecipePage = () => {
               enter the different steps seperated by 2 percentage signs ( %% )
             </Form.Text>
             <Form.Control
-              value={recipe.method?.join('%%')}
+              value={method?.join('%%')}
+              // value={method}
               onChange={e => setMethod(e.target.value.split('%%'))}
               type="text"
             />
@@ -125,39 +142,39 @@ const EditRecipePage = () => {
             <Col>
               <Form.Label>Prep Time</Form.Label>
               <Form.Select onChange={e => setPrepTime(e.target.value)}>
-                <option selected={recipe.prepTime === '0-15' ? true : false} value="0-15">0-15</option>
-                <option selected={recipe.prepTime === '15-30' ? true : false} value="15-30">15-30</option>
-                <option selected={recipe.prepTime === '30-60' ? true : false} value="30-60">30-60</option>
-                <option selected={recipe.prepTime === '60-90' ? true : false} value="60-90">60-90</option>
-                <option selected={recipe.prepTime === '90-120' ? true : false} value="90-120">90-120</option>
-                <option selected={recipe.prepTime === '>120' ? true : false} value=">120">>120</option>
+                <option selected={prepTime === '0-15' ? true : false} value="0-15">0-15</option>
+                <option selected={prepTime === '15-30' ? true : false} value="15-30">15-30</option>
+                <option selected={prepTime === '30-60' ? true : false} value="30-60">30-60</option>
+                <option selected={prepTime === '60-90' ? true : false} value="60-90">60-90</option>
+                <option selected={prepTime === '90-120' ? true : false} value="90-120">90-120</option>
+                <option selected={prepTime === '>120' ? true : false} value=">120">>120</option>
               </Form.Select>
             </Col>
             <Col>
               <Form.Label>Cooking Time</Form.Label>
               <Form.Select onChange={e => setCookTime(e.target.value)}>
-                <option selected={recipe.cookTime === '0-15' ? true : false} value="0-15">0-15</option>
-                <option selected={recipe.cookTime === '15-30' ? true : false} value="15-30">15-30</option>
-                <option selected={recipe.cookTime === '30-60' ? true : false} value="30-60">30-60</option>
-                <option selected={recipe.cookTime === '60-90' ? true : false} value="60-90">60-90</option>
-                <option selected={recipe.cookTime === '90-120' ? true : false} value="90-120">90-120</option>
-                <option selected={recipe.cookTime === '>120' ? true : false} value=">120">>120</option>
+                <option selected={cookTime === '0-15' ? true : false} value="0-15">0-15</option>
+                <option selected={cookTime === '15-30' ? true : false} value="15-30">15-30</option>
+                <option selected={cookTime === '30-60' ? true : false} value="30-60">30-60</option>
+                <option selected={cookTime === '60-90' ? true : false} value="60-90">60-90</option>
+                <option selected={cookTime === '90-120' ? true : false} value="90-120">90-120</option>
+                <option selected={cookTime === '>120' ? true : false} value=">120">>120</option>
               </Form.Select>
             </Col>
             <Col>
               <Form.Group className="mb-3" controlId="description">
                 <Form.Label>Serving Size</Form.Label>
-                <Form.Control value={recipe.servingSize} onChange={e => setServingSize(e.target.value)} type="text" placeholder="Serving Size" />
+                <Form.Control value={servingSize} onChange={e => setServingSize(e.target.value)} type="text" placeholder="Serving Size" />
               </Form.Group>
             </Col>
             <Col>
               <Form.Label>Difficulty</Form.Label>
               <Form.Select onChange={e => setDifficulty(e.target.value)}>
-                <option selected={recipe.difficulty === 1 ? true : false} value="1">1</option>
-                <option selected={recipe.difficulty === 2 ? true : false} value="2">2</option>
-                <option selected={recipe.difficulty === 3 ? true : false} value="3">3</option>
-                <option selected={recipe.difficulty === 4 ? true : false} value="4">4</option>
-                <option selected={recipe.difficulty === 5 ? true : false} value="5">5</option>
+                <option selected={difficulty === 1 ? true : false} value="1">1</option>
+                <option selected={difficulty === 2 ? true : false} value="2">2</option>
+                <option selected={difficulty === 3 ? true : false} value="3">3</option>
+                <option selected={difficulty === 4 ? true : false} value="4">4</option>
+                <option selected={difficulty === 5 ? true : false} value="5">5</option>
               </Form.Select>
             </Col>
           </Row>
